09da8d865fe3f065eae7a44be4b4a042003547e9
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-06-10 23:53:18 -0300

    Ajustado caminha da aplicação e suas dependencias

--- src/NerdStore.Core/Bus/MediatrHandler.cs
+++ src/NerdStore.Core/Bus/MediatrHandler.cs
@@ -8,6 +8,11 @@ namespace NerdStore.Core.Bus
     {
         private readonly IMediator _mediator;
 
+        public MediatrHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
         public async Task PublicarEvento<T>(T evento) where T : Event
         {
             await _mediator.Publish(evento);
