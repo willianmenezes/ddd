f2462a4a2b68fe893e53b1e5909ca7965cba24d9
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-06-10 23:44:03 -0300

    Criado camada de aplicação, criado mapeamento de entidades e viewmodels com automapper.

---
+++ NerdStore.Catalogo.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NerdStore.Catalogo.Application.ViewModels;
+
+namespace NerdStore.Catalogo.Application.Services
+{
+    public interface IProdutoAppService : IDisposable
+    {
+        Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int codigo);
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<IEnumerable<CategoriaViewModel>> ObterCatorias();
+
+        Task AdicionarProduto(ProdutoViewModel produto);
+        Task AtualizarProduto(ProdutoViewModel produto);
+
+        Task<ProdutoViewModel> DegbitarEstoque(Guid id, int quantidade);
+        Task<ProdutoViewModel> ReporEstoque(Guid id, int quantidade);
+    }
+}
