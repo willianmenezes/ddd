6b2f48103dee6e38aaff25e9567b496e39a8c6da
Willian Menezes <willian_menezes_santos@hotmail.com>
2021-06-28 00:11:55 -0300

    Adicionado classes de dominio para o contexto de vendas

--- src/NerdStore.Vendas.Domain/Pedido.cs
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -1,8 +1,39 @@
 ﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
 
 namespace NerdStore.Vendas.Domain
 {
     public class Pedido : Entity, IAggregateRoot
     {
+        public int Codigo { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Guid? VoucherId { get; set; }
+        public bool VoucherUtilizado { get; set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+        private readonly List<PedidoItem> _pedidoItens;
+        public IReadOnlyCollection<PedidoItem> PedidoItens => _pedidoItens;
+
+        // EF RELATION
+        public Voucher Voucher { get; private set; }
+
+        public Pedido(Guid clienteId, bool voucherUtilizado, decimal desconto, decimal valorTotal)
+        {
+            ClienteId = clienteId;
+            VoucherUtilizado = voucherUtilizado;
+            Desconto = desconto;
+            ValorTotal = valorTotal;
+            _pedidoItens = new List<PedidoItem>();
+        }
+
+        protected Pedido()
+        {
+            _pedidoItens = new List<PedidoItem>();
+        }
+
+
     }
 }
